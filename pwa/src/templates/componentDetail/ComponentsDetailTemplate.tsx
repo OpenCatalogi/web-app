8195e71d43464bbaa77953d1c6266ff12317a031
Lennart <66552932+lencodes@users.noreply.github.com>
2022-06-28 12:36:43 +0200

    ADD layoutClassName to container

--- pwa/src/templates/componentDetail/ComponentsDetailTemplate.tsx
+++ pwa/src/templates/componentDetail/ComponentsDetailTemplate.tsx
@@ -27,41 +27,37 @@ export const ComponentsDetailTemplate: React.FC<ComponentsDetailTemplateProps> =
   }, [getComponents.isSuccess]);
 
   return (
-    <Container>
-      <div className={styles.container}>
-        <div className={styles.backButton} onClick={() => navigate("/components")}>
-          <Link icon={<ArrowLeftIcon />} iconAlign="start">
-            {t("Back to components")}
-          </Link>
-        </div>
-
-        {!getComponents.isLoading && component && (
-          <>
-            <div className={styles.heading}>
-              <div>
-                <Heading1>{component.name}</Heading1>
-                <span className={styles.subtitle}>
-                  Phasellus tempus. Aenean vulputate eleifend tellus. Sed a libero.
-                </span>
-              </div>
-              <div className={styles.tags}>
-                <Tag tag={component.intendedAudience} />
-                <Tag tag={component.developmentStatus} />
-              </div>
-            </div>
-
-            <Paragraph className={styles.description}>{component.description}</Paragraph>
-
-            <a className={styles.externalLink} href={component.isBasedOn} target="_blank">
-              <Link icon={<ArrowRightIcon />} iconAlign="start">
-                {t("View component on GitHub")}
-              </Link>
-            </a>
-          </>
-        )}
-
-        {getComponents.isLoading && <Skeleton height="250px" />}
+    <Container layoutClassName={styles.container}>
+      <div className={styles.backButton} onClick={() => navigate("/components")}>
+        <Link icon={<ArrowLeftIcon />} iconAlign="start">
+          {t("Back to components")}
+        </Link>
       </div>
+
+      {!getComponents.isLoading && component && (
+        <>
+          <div className={styles.heading}>
+            <div>
+              <Heading1>{component.name}</Heading1>
+              <span className={styles.subtitle}>Phasellus tempus. Aenean vulputate eleifend tellus. Sed a libero.</span>
+            </div>
+            <div className={styles.tags}>
+              <Tag tag={component.intendedAudience} />
+              <Tag tag={component.developmentStatus} />
+            </div>
+          </div>
+
+          <Paragraph className={styles.description}>{component.description}</Paragraph>
+
+          <a className={styles.externalLink} href={component.isBasedOn} target="_blank">
+            <Link icon={<ArrowRightIcon />} iconAlign="start">
+              {t("View component on GitHub")}
+            </Link>
+          </a>
+        </>
+      )}
+
+      {getComponents.isLoading && <Skeleton height="250px" />}
     </Container>
   );
 };
