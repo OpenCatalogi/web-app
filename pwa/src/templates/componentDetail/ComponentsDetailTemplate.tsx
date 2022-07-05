93697aa67ee44588940bf9ea397c964a06601fbb
Lennart <66552932+lencodes@users.noreply.github.com>
2022-07-05 16:09:08 +0200

    REMOVE useComponent hook in detail page

--- pwa/src/templates/componentDetail/ComponentsDetailTemplate.tsx
+++ pwa/src/templates/componentDetail/ComponentsDetailTemplate.tsx
@@ -4,27 +4,16 @@ import { Heading1, Link, Paragraph } from "@gemeente-denhaag/components-react";
 import { Container, Tag } from "@conduction/components";
 import { navigate } from "gatsby";
 import { ArrowLeftIcon, ArrowRightIcon } from "@gemeente-denhaag/icons";
-import { useComponent } from "../../hooks/components";
-import Skeleton from "react-loading-skeleton";
 import { useTranslation } from "react-i18next";
+import { components as c } from "./../../testData/components";
 
 interface ComponentsDetailTemplateProps {
   componentId: string;
 }
 
 export const ComponentsDetailTemplate: React.FC<ComponentsDetailTemplateProps> = ({ componentId }) => {
-  const [component, setComponent] = React.useState<any>(null);
+  const [component] = React.useState<any>(c.find((_c) => _c.id === componentId));
   const { t } = useTranslation();
-
-  const _useComponent = useComponent();
-  const getComponents = _useComponent.getAll();
-
-  React.useEffect(() => {
-    if (!getComponents.isSuccess) return;
-
-    const _component = getComponents.data.find((c: any) => c.id === componentId);
-    setComponent(_component);
-  }, [getComponents.isSuccess]);
 
   return (
     <Container layoutClassName={styles.container}>
@@ -34,30 +23,24 @@ export const ComponentsDetailTemplate: React.FC<ComponentsDetailTemplateProps> =
         </Link>
       </div>
 
-      {!getComponents.isLoading && component && (
-        <>
-          <div className={styles.heading}>
-            <div>
-              <Heading1>{component.name}</Heading1>
-              <span className={styles.subtitle}>Phasellus tempus. Aenean vulputate eleifend tellus. Sed a libero.</span>
-            </div>
-            <div className={styles.tags}>
-              <Tag tag={component.intendedAudience} />
-              <Tag tag={component.developmentStatus} />
-            </div>
-          </div>
+      <div className={styles.heading}>
+        <div>
+          <Heading1>{component.name}</Heading1>
+          <span className={styles.subtitle}>Phasellus tempus. Aenean vulputate eleifend tellus. Sed a libero.</span>
+        </div>
+        <div className={styles.tags}>
+          <Tag tag={component.layer} />
+          <Tag tag={component.status} />
+        </div>
+      </div>
 
-          <Paragraph className={styles.description}>{component.description}</Paragraph>
+      <Paragraph className={styles.description}>{component.description}</Paragraph>
 
-          <a className={styles.externalLink} href={component.isBasedOn} target="_blank">
-            <Link icon={<ArrowRightIcon />} iconAlign="start">
-              {t("View component on GitHub")}
-            </Link>
-          </a>
-        </>
-      )}
-
-      {getComponents.isLoading && <Skeleton height="250px" />}
+      <a className={styles.externalLink} href={component.isBasedOn} target="_blank">
+        <Link icon={<ArrowRightIcon />} iconAlign="start">
+          {t("View component on GitHub")}
+        </Link>
+      </a>
     </Container>
   );
 };
