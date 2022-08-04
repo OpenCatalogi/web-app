ed333a9d8f7d9828b7717155b6734e5af8779cc8
Lencodes <contact@lencodes.com>
2022-08-04 15:13:34 +0200

    REFACTOR URI with /oc prefix

--- pwa/static/env.js
+++ pwa/static/env.js
@@ -11,7 +11,7 @@ export const setEnv = () => {
 
   if (process.env.NODE_ENV === "production") {
     window.GATSBY_ME_URL = "https://opencatalogi.nl/api/users/me";
-    window.GATSBY_API_URL = "https://opencatalogi.nl/api";
+    window.GATSBY_API_URL = "https://opencatalogi.nl/api/oc";
     window.GATSBY_ADMIN_URL = "https://opencatalogi.nl/admin";
     window.GATSBY_BASE_URL = "https://opencatalogi.nl";
     window.GATSBY_FRONTEND_URL = "https://opencatalogi.nl";
