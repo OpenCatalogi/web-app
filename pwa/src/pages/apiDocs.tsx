a03996733061fcf2f72a1857dc96a39cde508f55
Remko <remko@conduction.nl>
2022-08-05 11:18:56 +0200

    added requested changes

--- pwa/src/pages/apiDocs.tsx
+++
@@ -1,9 +0,0 @@
-import { PageProps } from "gatsby";
-import * as React from "react";
-import { ApiDocsTemplate } from "../templates/apiDocs/ApiDocsTemplate";
-
-const IndexPage: React.FC<PageProps> = () => {
-  return <ApiDocsTemplate />;
-};
-
-export default IndexPage;
--- pwa/src/templates/documentation/apiDocumentation/ApiDocumentationTemplate.tsx
+++ pwa/src/templates/documentation/apiDocumentation/ApiDocumentationTemplate.tsx
@@ -1,22 +1,20 @@
 import * as React from "react";
-import * as styles from "./ApiDocsTemplate.module.css";
+import * as styles from "./ApiDocumentationTemplate.module.css";
 import { Container } from "@conduction/components";
 import { Heading1, LeadParagraph, Link } from "@gemeente-denhaag/components-react";
 import { navigate } from "gatsby";
 import { ExternalLinkIcon } from "@gemeente-denhaag/icons";
 
-export const ApiDocsTemplate: React.FC = () => {
+export const ApiDocumentationTemplate: React.FC = () => {
   return (
     <Container layoutClassName={styles.container}>
       <section className={styles.section}>
-        <Heading1>Api</Heading1>
+        <Heading1>API Documentatie</Heading1>
 
         <LeadParagraph>
           Quisque id mi. Vivamus in erat ut urna cursus vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus
-          et ultrices posuere cubilia Curae Sed aliquam, nisi quis porttitor congue, elit erat euismod orci, ac placerat
-          dolor lectus quis orci.
+          et ultrices posuere cubilia Curae Sed aliquam.{" "}
           <span
-            className={styles.link}
             onClick={() => {
               navigate("#");
             }}
@@ -24,7 +22,8 @@ export const ApiDocsTemplate: React.FC = () => {
             <Link icon={<ExternalLinkIcon />} iconAlign="start">
               Lees meer.
             </Link>
-          </span>
+          </span>{" "}
+          Nisi quis porttitor congue, elit erat euismod orci, ac placerat dolor lectus quis orci.
         </LeadParagraph>
       </section>
     </Container>
