b92027319e4ecac72d6d11bc4542444836ae9ba6
Remko <remko@conduction.nl>
2022-08-31 15:54:24 +0200

    styled the theme switcher

---
+++ pwa/src/pages/components/formFields/types.ts
@@ -0,0 +1,7 @@
+import { FieldErrors, FieldValues, RegisterOptions, UseFormRegister } from "react-hook-form";
+
+export interface IReactHookFormProps {
+  register: UseFormRegister<FieldValues>;
+  errors: FieldErrors;
+  validation?: Omit<RegisterOptions<FieldValues, any>, "valueAsNumber" | "valueAsDate" | "setValueAs" | "disabled">;
+}
