c639589a1777ecd4f43de1f693b63f78edcdd652
Remko <remko@conduction.nl>
2022-09-14 15:36:03 +0200

    added circles and more colors

--- pwa/src/templates/templateParts/dependenciesTemplates/networkDependenciesTemplate/NetworkDependenciesTemplate.tsx
+++ pwa/src/templates/templateParts/dependenciesTemplates/networkDependenciesTemplate/NetworkDependenciesTemplate.tsx
@@ -3,13 +3,11 @@ import * as styles from "./NetworkDependenciesTemplate.module.css";
 import { Network } from "vis-network";
 import { useTranslation } from "react-i18next";
 import _ from "lodash";
-
 import { getTokenValue } from "../../../../services/getTokenValue";
 import { addNewLineToString } from "../../../../services/addNewLineToString";
 
 interface LayersResultTemplateProps {
   components: any[];
-  hideTableHead?: boolean;
   mainComponent: {
     id: string;
     name: string;
@@ -17,11 +15,7 @@ interface LayersResultTemplateProps {
   };
 }
 
-export const NetworkDependenciesTemplate: React.FC<LayersResultTemplateProps> = ({
-  components,
-  hideTableHead,
-  mainComponent,
-}) => {
+export const NetworkDependenciesTemplate: React.FC<LayersResultTemplateProps> = ({ components, mainComponent }) => {
   const { t } = useTranslation();
 
   const container = React.useRef(null);
@@ -30,18 +24,52 @@ export const NetworkDependenciesTemplate: React.FC<LayersResultTemplateProps> = 
     id: component.id,
     label: addNewLineToString(component.name),
     layer: component.embedded?.nl?.embedded?.commonground?.layerType,
-    color: getTokenValue(
-      styles[_.camelCase(`layerColor ${t(component.embedded?.nl?.embedded?.commonground?.layerType)}`)],
-    ),
-    font: { color: "white" },
+    color: {
+      background: getTokenValue(
+        styles[_.camelCase(`layerColor ${t(_.upperFirst(component.embedded?.nl?.embedded?.commonground?.layerType))}`)],
+      ),
+      border: getTokenValue(
+        styles[
+          _.camelCase(`borderLayerColor ${t(_.upperFirst(component.embedded?.nl?.embedded?.commonground?.layerType))}`)
+        ],
+      ),
+      highlight: {
+        background: getTokenValue(
+          styles[
+            _.camelCase(
+              `highlightLayerColor ${t(_.upperFirst(component.embedded?.nl?.embedded?.commonground?.layerType))}`,
+            )
+          ],
+        ),
+        border: getTokenValue(
+          styles[
+            _.camelCase(`layerColor ${t(_.upperFirst(component.embedded?.nl?.embedded?.commonground?.layerType))}`)
+          ],
+        ),
+      },
+    },
+    font: {
+      color: "white",
+      size: 10,
+      highlight: {
+        color: "black",
+      },
+    },
   }));
 
   const mainComponentNode = {
     id: mainComponent.id,
     label: addNewLineToString(mainComponent.name),
     layer: mainComponent.layer,
-    color: getTokenValue(styles[_.camelCase(`layerColor ${t(mainComponent.layer)}`)]),
-    font: { color: "white" },
+    color: {
+      background: getTokenValue(styles[_.camelCase(`layerColor ${t(_.upperFirst(mainComponent.layer))}`)]),
+      border: getTokenValue(styles[_.camelCase(`borderLayerColor ${t(_.upperFirst(mainComponent.layer))}`)]),
+      highlight: {
+        background: getTokenValue(styles[_.camelCase(`highlightLayerColor ${t(_.upperFirst(mainComponent.layer))}`)]),
+        border: getTokenValue(styles[_.camelCase(`layerColor ${t(_.upperFirst(mainComponent.layer))}`)]),
+      },
+    },
+    font: { color: "white", size: 20 },
   };
 
   const nodes = [mainComponentNode, ...componentNodes];
@@ -53,23 +81,29 @@ export const NetworkDependenciesTemplate: React.FC<LayersResultTemplateProps> = 
     return {
       from: component.id,
       to: mainComponent.id,
-      arrows: "from",
     };
   });
 
   const options = {
     nodes: {
-      shape: "box",
-      size: 16,
+      shape: "circle",
+      margin: {
+        top: 10,
+        left: 10,
+        right: 10,
+        bottom: 10,
+      },
+      borderWidth: 2,
+      borderWidthSelected: 1,
     },
     edges: {
-      color: "black",
+      color: "darkGray",
     },
     physics: {
       forceAtlas2Based: {
         gravitationalConstant: -26,
         centralGravity: 0.005,
-        springLength: nodes.length > 10 ? 250 : 100,
+        springLength: 250,
         springConstant: 0.18,
       },
       maxVelocity: 146,
@@ -83,5 +117,5 @@ export const NetworkDependenciesTemplate: React.FC<LayersResultTemplateProps> = 
     container.current && new Network(container.current, { nodes, edges }, options);
   }, [container, nodes, edges]);
 
-  return <div ref={container} className={styles.test} />;
+  return <div ref={container} className={styles.networkContainer} />;
 };
