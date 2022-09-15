b400b52adbdfabf26abc280a368a8c4abfbdc7f2
Remko <remko@conduction.nl>
2022-09-15 10:46:05 +0200

    ComponentCard implementation fix

--- pwa/src/templates/templateParts/componentCardsAccordion/ComponentCardsAccordionTemplate.tsx
+++ pwa/src/templates/templateParts/componentCardsAccordion/ComponentCardsAccordionTemplate.tsx
@@ -148,7 +148,7 @@ const Components: React.FC<ComponentsProps> = ({ components }) => {
           category={{ label: "functie autorisatie", icon: <FontAwesomeIcon icon={faLock} /> }}
           tags={{
             status: component.developmentStatus,
-            installations: component.usedBy?.length(),
+            installations: component.usedBy?.length.toString() ?? "0",
             organization: component?.embedded?.legal?.embedded?.repoOwner.name,
             licence: component?.embedded?.legal?.license,
             githubLink: component?.embedded?.url?.url,
