c6707db4e672660d7874786da294ffd48b7fe667
Remko <remko@conduction.nl>
2022-09-16 10:19:33 +0200

    added requested changes

--- pwa/src/templates/organizationDetail/OrganizationDetailTemplate.tsx
+++ pwa/src/templates/organizationDetail/OrganizationDetailTemplate.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import * as styles from "./OrganizationDetailTemplate.module.css";
-import { Container, QuoteWrapper } from "@conduction/components";
+import { Container } from "@conduction/components";
 import {
   Divider,
   Heading1,
@@ -12,7 +12,6 @@ import {
   Tabs,
 } from "@gemeente-denhaag/components-react";
 import { GitHubLogo } from "../../assets/svgs/GitHub";
-import organizationLogo from "./../../assets/svgs/LogoRotterdam.svg";
 import { ComponentCardsAccordionTemplate } from "../templateParts/componentCardsAccordion/ComponentCardsAccordionTemplate";
 import { BadgeCounter } from "../../components/badgeCounter/BadgeCounter";
 import { TEMPORARY_COMPONENTS } from "../../data/components";
@@ -37,285 +36,129 @@ export const OrganizationDetailTemplate: React.FC<OrganizationDetailTemplateProp
   const TempComponentsSupported = TEMPORARY_COMPONENTS.slice(8, 13);
   const TempComponentsUsed = TEMPORARY_COMPONENTS;
 
-  const tempOrganization = TEMPORARY_ORGANIZATIONS.find((organization) => {
-    return organization.id === organizationId;
-  });
+  const tempOrganization =
+    TEMPORARY_ORGANIZATIONS.find((organization) => organization.id === organizationId) ?? TEMPORARY_ORGANIZATIONS[1];
 
   return (
     <Container layoutClassName={styles.container}>
-      {tempOrganization && (
-        <>
-          <div className={styles.headerContainer}>
-            <div className={styles.headerContent}>
-              <Heading1>{tempOrganization.name}</Heading1>
+      <div className={styles.headerContainer}>
+        <div className={styles.headerContent}>
+          <Heading1>{tempOrganization.name}</Heading1>
 
-              <LeadParagraph>{tempOrganization.description}</LeadParagraph>
-            </div>
+          <LeadParagraph>{tempOrganization.description}</LeadParagraph>
+        </div>
 
-            <div className={styles.headerOrganizationData}>
-              <div className={styles.logoContainer}>
-                <img className={styles.logo} src={tempOrganization.logo} alt="Organization logo" />
-              </div>
-              <div>
-                <div className={styles.tagsContainer}>
-                  {tempOrganization.github && (
-                    <ToolTip tooltip="GitHub">
-                      <Tag label={t("GitHub")} icon={<GitHubLogo />} onClick={() => open(tempOrganization.github)} />
-                    </ToolTip>
-                  )}
-                  {tempOrganization.gitlab && (
-                    <ToolTip tooltip="GitLab">
-                      <Tag label={t("GitLab")} icon={<GitLabLogo />} onClick={() => open(tempOrganization.gitlab)} />
-                    </ToolTip>
-                  )}
-                  {tempOrganization.website && (
-                    <ToolTip tooltip={"Website"}>
-                      <Tag
-                        label={tempOrganization.website}
-                        icon={<FontAwesomeIcon icon={faGlobe} />}
-                        onClick={() => open(tempOrganization.website)}
-                      />
-                    </ToolTip>
-                  )}
-                  {tempOrganization.phone && (
-                    <ToolTip tooltip={"Telefoonnummer"}>
-                      <Tag
-                        label={tempOrganization.phone}
-                        icon={<FontAwesomeIcon icon={faPhone} />}
-                        onClick={() => navigate(`tel:${tempOrganization.phone}`)}
-                      />
-                    </ToolTip>
-                  )}
-                  {tempOrganization.email && (
-                    <ToolTip tooltip={"EmailAddress"}>
-                      <Tag
-                        label={tempOrganization.email}
-                        icon={<FontAwesomeIcon icon={faEnvelope} />}
-                        onClick={() => navigate(`mailto:${tempOrganization.email}`)}
-                      />
-                    </ToolTip>
-                  )}
-                </div>
-              </div>
+        <div className={styles.headerOrganizationData}>
+          <div className={styles.logoContainer}>
+            <img className={styles.logo} src={tempOrganization.logo} alt="Organization logo" />
+          </div>
+          <div>
+            <div className={styles.tagsContainer}>
+              {tempOrganization.github && (
+                <ToolTip tooltip="GitHub">
+                  <Tag label={t("GitHub")} icon={<GitHubLogo />} onClick={() => open(tempOrganization.github)} />
+                </ToolTip>
+              )}
+              {tempOrganization.gitlab && (
+                <ToolTip tooltip="GitLab">
+                  <Tag label={t("GitLab")} icon={<GitLabLogo />} onClick={() => open(tempOrganization.gitlab)} />
+                </ToolTip>
+              )}
+              {tempOrganization.website && (
+                <ToolTip tooltip={"Website"}>
+                  <Tag
+                    label={tempOrganization.website}
+                    icon={<FontAwesomeIcon icon={faGlobe} />}
+                    onClick={() => open(tempOrganization.website)}
+                  />
+                </ToolTip>
+              )}
+              {tempOrganization.phone && (
+                <ToolTip tooltip={"Telefoonnummer"}>
+                  <Tag
+                    label={tempOrganization.phone}
+                    icon={<FontAwesomeIcon icon={faPhone} />}
+                    onClick={() => navigate(`tel:${tempOrganization.phone}`)}
+                  />
+                </ToolTip>
+              )}
+              {tempOrganization.email && (
+                <ToolTip tooltip={"EmailAddress"}>
+                  <Tag
+                    label={tempOrganization.email}
+                    icon={<FontAwesomeIcon icon={faEnvelope} />}
+                    onClick={() => navigate(`mailto:${tempOrganization.email}`)}
+                  />
+                </ToolTip>
+              )}
             </div>
           </div>
+        </div>
+      </div>
 
-          <Divider />
+      <Divider />
 
-          <div className={styles.section}>
-            <Heading2>Componenten</Heading2>
-            <TabContext value={currentTab.toString()}>
-              <Tabs
-                value={currentTab}
-                onChange={(_, newValue: number) => {
-                  setCurrentTab(newValue);
-                }}
-                variant="scrollable"
-              >
-                <Tab
-                  className={styles.tab}
-                  label={
-                    <BadgeCounter
-                      layoutClassName={styles.tabAmountBadge}
-                      number={_.toString(TempComponentsOwned.length)}
-                    >
-                      Eigen componenten
-                    </BadgeCounter>
-                  }
-                  value={0}
-                />
-                <Tab
-                  className={styles.tab}
-                  label={
-                    <BadgeCounter
-                      layoutClassName={styles.tabAmountBadge}
-                      number={_.toString(TempComponentsSupported.length)}
-                    >
-                      Ondersteunde componenten
-                    </BadgeCounter>
-                  }
-                  value={1}
-                />
-                <Tab
-                  className={styles.tab}
-                  label={
-                    <BadgeCounter
-                      layoutClassName={styles.tabAmountBadge}
-                      number={_.toString(TempComponentsUsed.length)}
-                    >
-                      Gebruikte componenten
-                    </BadgeCounter>
-                  }
-                  value={2}
-                />
-              </Tabs>
+      <div className={styles.section}>
+        <Heading2>Componenten</Heading2>
+        <TabContext value={currentTab.toString()}>
+          <Tabs
+            value={currentTab}
+            onChange={(_, newValue: number) => {
+              setCurrentTab(newValue);
+            }}
+            variant="scrollable"
+          >
+            <Tab
+              className={styles.tab}
+              label={
+                <BadgeCounter layoutClassName={styles.tabAmountBadge} number={_.toString(TempComponentsOwned.length)}>
+                  Eigen componenten
+                </BadgeCounter>
+              }
+              value={0}
+            />
+            <Tab
+              className={styles.tab}
+              label={
+                <BadgeCounter
+                  layoutClassName={styles.tabAmountBadge}
+                  number={_.toString(TempComponentsSupported.length)}
+                >
+                  Ondersteunde componenten
+                </BadgeCounter>
+              }
+              value={1}
+            />
+            <Tab
+              className={styles.tab}
+              label={
+                <BadgeCounter layoutClassName={styles.tabAmountBadge} number={_.toString(TempComponentsUsed.length)}>
+                  Gebruikte componenten
+                </BadgeCounter>
+              }
+              value={2}
+            />
+          </Tabs>
 
-              <TabPanel className={styles.tabPanel} value="0">
-                <div className={styles.components}>
-                  <ComponentCardsAccordionTemplate components={TempComponentsOwned} />
-                </div>
-              </TabPanel>
-
-              <TabPanel className={styles.tabPanel} value="1">
-                <div className={styles.components}>
-                  <ComponentCardsAccordionTemplate components={TempComponentsSupported} />
-                </div>
-              </TabPanel>
-
-              <TabPanel className={styles.tabPanel} value="2">
-                <div className={styles.components}>
-                  <ComponentCardsAccordionTemplate components={TempComponentsUsed} />
-                </div>
-              </TabPanel>
-            </TabContext>
-          </div>
-
-        </>
-      )}
-      {!tempOrganization && (
-        <>
-          <div className={styles.headerContainer}>
-            <div className={styles.headerContent}>
-              <Heading1>Gemeente Rotterdam</Heading1>
-
-              <LeadParagraph>
-                <QuoteWrapper>
-                  Een stad waar je ruimte ervaart. <br />
-                  Bij een organisatie die je vrijheid en verantwoordelijkheid biedt. <br />
-                  Voor werk waar je uitdaging voelt. <br />
-                  Zodat jij en de stad blijven groeien. <br />
-                  Voor en met alle Rotterdammers.
-                </QuoteWrapper>
-                <br />
-                Bij een stad die je elke dag uitdaagt en het beste in jou naar boven haalt, hoort een eigentijdse
-                overheid die meebeweegt met de energie van de stad. Zo’n organisatie wil de gemeente Rotterdam zijn.
-                Daarom bieden we je ruimte en kansen om Rotterdam en daarmee jezelf vooruit te blijven helpen.
-                <br />
-                <br />
-                Wij creëren de voorwaarden zodat jij de ruimte ervaart om te groeien: in de stad en binnen de eigen
-                organisatie. In een omgeving met collega’s die veilig en vertrouwd voelt. Waar we samen beslissen en
-                samen aanpakken.
-              </LeadParagraph>
+          <TabPanel className={styles.tabPanel} value="0">
+            <div className={styles.components}>
+              <ComponentCardsAccordionTemplate components={TempComponentsOwned} />
             </div>
+          </TabPanel>
 
-            <div className={styles.headerOrganizationData}>
-              <div className={styles.logoContainer}>
-                <img className={styles.oldLogo} src={organizationLogo} alt="Organization logo" />
-              </div>
-              <div>
-                <div className={styles.tagsContainer}>
-                  <ToolTip tooltip="GitHub">
-                    <Tag
-                      label={t("GitHub")}
-                      icon={<GitHubLogo />}
-                      onClick={() => open("https://github.com/OpenCatalogi")}
-                    />
-                  </ToolTip>
-                  <ToolTip tooltip="GitLab">
-                    <Tag
-                      label={t("GitLab")}
-                      icon={<GitLabLogo />}
-                      onClick={() => open("https://github.com/OpenCatalogi")}
-                    />
-                  </ToolTip>
-
-                  <ToolTip tooltip={"Website"}>
-                    <Tag
-                      label={"https://www.rotterdam.nl"}
-                      icon={<FontAwesomeIcon icon={faGlobe} />}
-                      onClick={() => open("https://www.rotterdam.nl")}
-                    />
-                  </ToolTip>
-                  <ToolTip tooltip={"Telefoonnummer"}>
-                    <Tag
-                      label={"+31 10 267 16 25"}
-                      icon={<FontAwesomeIcon icon={faPhone} />}
-                      onClick={() => navigate(`tel:${"+31 10 267 16 25"}`)}
-                    />
-                  </ToolTip>
-                  <ToolTip tooltip={"EmailAddress"}>
-                    <Tag
-                      label={"info@rotterdam.nl"}
-                      icon={<FontAwesomeIcon icon={faEnvelope} />}
-                      onClick={() => navigate(`mailto:${"info@rotterdam.nl"}`)}
-                    />
-                  </ToolTip>
-                </div>
-              </div>
+          <TabPanel className={styles.tabPanel} value="1">
+            <div className={styles.components}>
+              <ComponentCardsAccordionTemplate components={TempComponentsSupported} />
             </div>
-          </div>
+          </TabPanel>
 
-          <Divider />
-
-          <div className={styles.section}>
-            <Heading2>Componenten</Heading2>
-            <TabContext value={currentTab.toString()}>
-              <Tabs
-                value={currentTab}
-                onChange={(_, newValue: number) => {
-                  setCurrentTab(newValue);
-                }}
-                variant="scrollable"
-              >
-                <Tab
-                  className={styles.tab}
-                  label={
-                    <BadgeCounter
-                      layoutClassName={styles.tabAmountBadge}
-                      number={_.toString(TempComponentsOwned.length)}
-                    >
-                      Eigen componenten
-                    </BadgeCounter>
-                  }
-                  value={0}
-                />
-                <Tab
-                  className={styles.tab}
-                  label={
-                    <BadgeCounter
-                      layoutClassName={styles.tabAmountBadge}
-                      number={_.toString(TempComponentsSupported.length)}
-                    >
-                      Ondersteunde componenten
-                    </BadgeCounter>
-                  }
-                  value={1}
-                />
-                <Tab
-                  className={styles.tab}
-                  label={
-                    <BadgeCounter
-                      layoutClassName={styles.tabAmountBadge}
-                      number={_.toString(TempComponentsUsed.length)}
-                    >
-                      Gebruikte componenten
-                    </BadgeCounter>
-                  }
-                  value={2}
-                />
-              </Tabs>
-
-              <TabPanel className={styles.tabPanel} value="0">
-                <div className={styles.components}>
-                  <ComponentCardsAccordionTemplate components={TempComponentsOwned} />
-                </div>
-              </TabPanel>
-
-              <TabPanel className={styles.tabPanel} value="1">
-                <div className={styles.components}>
-                  <ComponentCardsAccordionTemplate components={TempComponentsSupported} />
-                </div>
-              </TabPanel>
-
-              <TabPanel className={styles.tabPanel} value="2">
-                <div className={styles.components}>
-                  <ComponentCardsAccordionTemplate components={TempComponentsUsed} />
-                </div>
-              </TabPanel>
-            </TabContext>
-          </div>
-        </>
-      )}
+          <TabPanel className={styles.tabPanel} value="2">
+            <div className={styles.components}>
+              <ComponentCardsAccordionTemplate components={TempComponentsUsed} />
+            </div>
+          </TabPanel>
+        </TabContext>
+      </div>
     </Container>
   );
 };
