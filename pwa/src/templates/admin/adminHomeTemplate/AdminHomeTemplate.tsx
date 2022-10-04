b16005e8f984e9d4ecf6bae861a7cd79caced737
Thijn <Ralkey@outlook.com>
2022-10-04 15:34:17 +0200

    removed "admin" from title and added table's

--- pwa/src/templates/admin/adminHomeTemplate/AdminHomeTemplate.tsx
+++ pwa/src/templates/admin/adminHomeTemplate/AdminHomeTemplate.tsx
@@ -3,13 +3,49 @@ import * as styles from "./AdminHomeTemplate.module.css";
 import { Container } from "@conduction/components";
 import { Heading1 } from "@gemeente-denhaag/components-react";
 import { AdminTemplate } from "../../templateParts/adminTemplate/AdminTemplate";
+import { t } from "i18next";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@gemeente-denhaag/table";
 
 export const AdminHomeTemplate: React.FC = () => {
   return (
     <AdminTemplate>
       <Container layoutClassName={styles.container}>
         <section className={styles.section}>
-          <Heading1>Admin Home</Heading1>
+          <Heading1>Home</Heading1>
+        </section>
+        <section className={styles.section}>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableHeader>{t("Name")}</TableHeader>
+                <TableHeader>{t("Layer")}</TableHeader>
+                <TableHeader>{t("Status")}</TableHeader>
+                <TableHeader>{t("Type")}</TableHeader>
+                <TableHeader>{t("Organization")}</TableHeader>
+                <TableHeader>{t("Installations")}</TableHeader>
+                <TableHeader />
+              </TableRow>
+            </TableHead>
+
+            <TableBody>
+              <TableRow>
+                <TableCell>#</TableCell>
+                <TableCell>#</TableCell>
+                <TableCell>#</TableCell>
+                <TableCell>#</TableCell>
+                <TableCell>#</TableCell>
+                <TableCell>#</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>#</TableCell>
+                <TableCell>#</TableCell>
+                <TableCell>#</TableCell>
+                <TableCell>#</TableCell>
+                <TableCell>#</TableCell>
+                <TableCell>#</TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
         </section>
       </Container>
     </AdminTemplate>
--- pwa/src/templates/admin/adminUserTemplate/AdminUserTemplate.tsx
+++ pwa/src/templates/admin/adminUserTemplate/AdminUserTemplate.tsx
@@ -3,13 +3,49 @@ import * as styles from "./AdminUserTemplate.module.css";
 import { Container } from "@conduction/components";
 import { Heading1 } from "@gemeente-denhaag/components-react";
 import { AdminTemplate } from "../../templateParts/adminTemplate/AdminTemplate";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@gemeente-denhaag/table";
+import { t } from "i18next";
 
 export const AdminUserTemplate: React.FC = () => {
   return (
     <AdminTemplate>
       <Container layoutClassName={styles.container}>
         <section className={styles.section}>
-          <Heading1>Admin User</Heading1>
+          <Heading1>User</Heading1>
+        </section>
+        <section className={styles.section}>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableHeader>{t("Name")}</TableHeader>
+                <TableHeader>{t("Layer")}</TableHeader>
+                <TableHeader>{t("Status")}</TableHeader>
+                <TableHeader>{t("Type")}</TableHeader>
+                <TableHeader>{t("Organization")}</TableHeader>
+                <TableHeader>{t("Installations")}</TableHeader>
+                <TableHeader />
+              </TableRow>
+            </TableHead>
+
+            <TableBody>
+              <TableRow>
+                <TableCell>#</TableCell>
+                <TableCell>#</TableCell>
+                <TableCell>#</TableCell>
+                <TableCell>#</TableCell>
+                <TableCell>#</TableCell>
+                <TableCell>#</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>#</TableCell>
+                <TableCell>#</TableCell>
+                <TableCell>#</TableCell>
+                <TableCell>#</TableCell>
+                <TableCell>#</TableCell>
+                <TableCell>#</TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
         </section>
       </Container>
     </AdminTemplate>
