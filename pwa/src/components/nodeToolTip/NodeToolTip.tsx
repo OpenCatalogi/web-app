32558fba79e0478728feab5be697726068673268
Thijn <thijn@conduction.nl>
2022-10-10 14:27:55 +0000

    Update NodeToolTip.tsx

--- pwa/src/components/nodeToolTip/NodeToolTip.tsx
+++ pwa/src/components/nodeToolTip/NodeToolTip.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import * as styles from "./ToolTip.module.css";
+import * as styles from "./NodeToolTip.module.css";
 import clsx from "clsx";
 
 interface NodeToolTipProps {
