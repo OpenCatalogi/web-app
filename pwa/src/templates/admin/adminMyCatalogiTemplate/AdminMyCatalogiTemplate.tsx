af7cc9fe270f624bb7cdc83fffa3be7865deb439
Remko <remko@conduction.nl>
2022-10-12 11:12:07 +0200

    added better data

--- pwa/src/templates/admin/adminMyCatalogiTemplate/AdminMyCatalogiTemplate.tsx
+++ pwa/src/templates/admin/adminMyCatalogiTemplate/AdminMyCatalogiTemplate.tsx
@@ -5,8 +5,18 @@ import { Heading1 } from "@gemeente-denhaag/components-react";
 import { AdminTemplate } from "../../templateParts/adminTemplate/AdminTemplate";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@gemeente-denhaag/table";
 import { t } from "i18next";
+import { TEMPORARY_COMPONENTS } from "../../../data/components";
+import { navigate } from "gatsby";
+import clsx from "clsx";
+import _ from "lodash";
+import { ToolTip } from "../../../components/toolTip/ToolTip";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Tag } from "../../../components/tag/Tag";
+import { faHouse, faInfoCircle, faLayerGroup, faRepeat } from "@fortawesome/free-solid-svg-icons";
 
 export const AdminMyCatalogiTemplate: React.FC = () => {
+  const components = TEMPORARY_COMPONENTS.slice(12, 15);
+
   return (
     <AdminTemplate>
       <Container>
@@ -28,22 +38,64 @@ export const AdminMyCatalogiTemplate: React.FC = () => {
             </TableHead>
 
             <TableBody>
-              <TableRow>
-                <TableCell>#</TableCell>
-                <TableCell>#</TableCell>
-                <TableCell>#</TableCell>
-                <TableCell>#</TableCell>
-                <TableCell>#</TableCell>
-                <TableCell>#</TableCell>
-              </TableRow>
-              <TableRow>
-                <TableCell>#</TableCell>
-                <TableCell>#</TableCell>
-                <TableCell>#</TableCell>
-                <TableCell>#</TableCell>
-                <TableCell>#</TableCell>
-                <TableCell>#</TableCell>
-              </TableRow>
+              {components.map((component) => (
+                <TableRow
+                  className={styles.tableRow}
+                  key={component.id}
+                  onClick={() => navigate(`/components/${component.id}`)}
+                >
+                  <TableCell>
+                    <span className={styles.name}>{component.name}</span>
+                  </TableCell>
+
+                  <TableCell>
+                    <div
+                      className={clsx(
+                        styles[_.camelCase(t(`${component.embedded?.nl.embedded.commonground.layerType} layer`))],
+                      )}
+                    >
+                      <ToolTip tooltip="Laag">
+                        <Tag
+                          layoutClassName={styles.tagWidth}
+                          label={t(_.upperFirst(component.embedded?.nl.embedded.commonground.layerType ?? "Onbekend"))}
+                          icon={<FontAwesomeIcon icon={faLayerGroup} />}
+                        />
+                      </ToolTip>
+                    </div>
+                  </TableCell>
+
+                  <TableCell>
+                    <ToolTip tooltip="Status">
+                      <Tag
+                        layoutClassName={styles.tagWidth}
+                        label={_.upperFirst(component.developmentStatus ?? "Onbekend")}
+                        icon={<FontAwesomeIcon icon={faInfoCircle} />}
+                      />
+                    </ToolTip>
+                  </TableCell>
+
+                  <TableCell>
+                    <ToolTip tooltip="Type">
+                      <Tag label={_.upperFirst(component.softwareType ?? "Onbekend")} />
+                    </ToolTip>
+                  </TableCell>
+
+                  <TableCell>
+                    <ToolTip tooltip="Organisatie">
+                      <Tag label={_.upperFirst("Onbekend")} icon={<FontAwesomeIcon icon={faHouse} />} />
+                    </ToolTip>
+                  </TableCell>
+
+                  <TableCell>
+                    <ToolTip tooltip="Installaties">
+                      <Tag
+                        label={_.upperFirst(component.usedBy?.length.toString() ?? "0")}
+                        icon={<FontAwesomeIcon icon={faRepeat} />}
+                      />
+                    </ToolTip>
+                  </TableCell>
+                </TableRow>
+              ))}
             </TableBody>
           </Table>
         </section>
