4e5c8924be2c4d87336e02bf14df6757ba444daf
Lencodes <contact@lencodes.com>
2022-10-25 11:28:07 +0200

    Revert "MERGE"
    This reverts commit 42baaf6282bfdc4f4bd2995880512f3d5a3bc477, reversing
    changes made to 43ea99471fca62a78b66bc25f2108eed650f8467.

--- pwa/src/layout/Layout.tsx
+++ pwa/src/layout/Layout.tsx
@@ -11,7 +11,9 @@ import { FiltersProvider, IFilters, filters as _filters } from "../context/filte
 import { ThemeProvider } from "../styling/themeProvider/ThemeProvider";
 import { useTranslation } from "react-i18next";
 import _ from "lodash";
-import { Head } from "./Head";
+import { Breadcrumbs, Container } from "@conduction/components";
+
+const { setEnv } = require("./../../static/env.js");
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -27,6 +29,7 @@ const Layout: React.FC<LayoutProps> = ({ children, pageContext, location }) => {
   const { t } = useTranslation();
 
   React.useEffect(() => {
+    setEnv();
     setAPI(new APIService());
   }, []);
 
@@ -58,9 +61,7 @@ const Layout: React.FC<LayoutProps> = ({ children, pageContext, location }) => {
   if (!API) return <></>;
 
   return (
-    <>
-      <Head />
-
+    <div>
       <GatsbyProvider value={gatsbyContext}>
         <APIProvider value={API}>
           <FiltersProvider value={[filters, setFilters]}>
@@ -78,7 +79,7 @@ const Layout: React.FC<LayoutProps> = ({ children, pageContext, location }) => {
           </FiltersProvider>
         </APIProvider>
       </GatsbyProvider>
-    </>
+    </div>
   );
 };
 
--- pwa/static/env.js
+++ pwa/static/env.js
@@ -1,16 +1,21 @@
-window.sessionStorage.setItem(
-  "GATSBY_ME_URL",
-  "https://opencatalogi.nl/api/users/me"
-);
-window.sessionStorage.setItem(
-  "GATSBY_API_URL",
-  "https://opencatalogi.nl/api/oc"
-);
-window.sessionStorage.setItem(
-  "GATSBY_ADMIN_URL",
-  "https://opencatalogi.nl/admin"
-);
-window.sessionStorage.setItem("GATSBY_BASE_URL", "https://opencatalogi.nl");
-window.sessionStorage.setItem("GATSBY_FRONTEND_URL", "https://opencatalogi.nl");
-window.sessionStorage.setItem("GATSBY_ORGANIZATION", "");
-window.sessionStorage.setItem("GATSBY_LOGIN_REDIRECT", "vault");
+export const setEnv = () => {
+  if (process.env.NODE_ENV === "development") {
+    window.GATSBY_ME_URL = "https://opencatalogi.nl/api/users/me";
+    window.GATSBY_API_URL = "https://opencatalogi.nl/api/oc";
+    window.GATSBY_ADMIN_URL = "https://opencatalogi.nl/admin";
+    window.GATSBY_BASE_URL = "https://opencatalogi.nl";
+    window.GATSBY_FRONTEND_URL = "https://opencatalogi.nl";
+    window.GATSBY_ORGANIZATION = "";
+    window.GATSBY_LOGIN_REDIRECT = "vault";
+  }
+
+  if (process.env.NODE_ENV === "production") {
+    window.GATSBY_ME_URL = "https://opencatalogi.nl/api/users/me";
+    window.GATSBY_API_URL = "https://opencatalogi.nl/api/oc";
+    window.GATSBY_ADMIN_URL = "https://opencatalogi.nl/admin";
+    window.GATSBY_BASE_URL = "https://opencatalogi.nl";
+    window.GATSBY_FRONTEND_URL = "https://opencatalogi.nl";
+    window.GATSBY_ORGANIZATION = "";
+    window.GATSBY_LOGIN_REDIRECT = "vault";
+  }
+};
