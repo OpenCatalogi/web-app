530e129540915fd6c48ff0332c2f461ee34e0ebb
Thijn <Ralkey@outlook.com>
2022-10-27 13:31:44 +0200

    application uses real data and has pagination

--- pwa/src/templates/applicationsDetailTemplate/ApplicationsDetailTemplate.tsx
+++ pwa/src/templates/applicationsDetailTemplate/ApplicationsDetailTemplate.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import * as styles from "./ApplicationsDetailTemplate.module.css";
-import { Divider, Heading1, LeadParagraph, Link } from "@gemeente-denhaag/components-react";
+import { Divider, Heading1, Heading2, LeadParagraph, Link } from "@gemeente-denhaag/components-react";
 import { Container, Tag, ToolTip } from "@conduction/components";
 import { navigate } from "gatsby";
 import { ArrowLeftIcon } from "@gemeente-denhaag/icons";
@@ -10,9 +10,9 @@ import { faHouse } from "@fortawesome/free-solid-svg-icons";
 import { GitHubLogo } from "../../assets/svgs/GitHub";
 import { categories as _categories } from "../../data/filters";
 import Skeleton from "react-loading-skeleton";
-import { TEMPORARY_APPLICATIONS } from "../../data/applications";
-import grey from "../../assets/images/grey.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { QueryClient } from "react-query";
+import { useApplications } from "../../hooks/applications";
 
 interface ApplicationsDetailTemplateProps {
   applicationId: string;
@@ -21,9 +21,9 @@ interface ApplicationsDetailTemplateProps {
 export const ApplicationsDetailTemplate: React.FC<ApplicationsDetailTemplateProps> = ({ applicationId }) => {
   const { t } = useTranslation();
 
-  const tempApplications = TEMPORARY_APPLICATIONS.find((application) => {
-    return application.id === applicationId;
-  });
+  const queryClient = new QueryClient();
+  const _useApplications = useApplications(queryClient);
+  const getApplications = _useApplications.getOne(applicationId);
 
   return (
     <Container layoutClassName={styles.container}>
@@ -33,36 +33,46 @@ export const ApplicationsDetailTemplate: React.FC<ApplicationsDetailTemplateProp
         </Link>
       </div>
 
-      {tempApplications && (
+      {getApplications.isSuccess && (
         <>
           <div className={styles.header}>
             <div className={styles.description}>
-              <Heading1>{tempApplications.name}</Heading1>
-              <LeadParagraph className={styles.description}>{tempApplications.description}</LeadParagraph>
+              <Heading1>{getApplications.data.name}</Heading1>
+              <LeadParagraph className={styles.description}>{getApplications.data.description}</LeadParagraph>
 
               <div className={styles.layerAndCategoryContainer}>
-                <ToolTip tooltip="Organisatie">
-                  <Tag label={tempApplications.owner.fullName} icon={<FontAwesomeIcon icon={faHouse} />} />
-                </ToolTip>
+                {getApplications.data.embedded && (
+                  <ToolTip tooltip="Organisatie">
+                    <Tag
+                      label={getApplications.data.embedded?.owner.fullName}
+                      icon={<FontAwesomeIcon icon={faHouse} />}
+                    />
+                  </ToolTip>
+                )}
 
-                {tempApplications.demoUrl && (
+                {getApplications.data.demoUrl && (
                   <ToolTip tooltip="GitHub">
-                    <Tag label={t("Repository")} icon={<GitHubLogo />} onClick={() => open(tempApplications.demoUrl)} />
+                    <Tag
+                      label={t("Repository")}
+                      icon={<GitHubLogo />}
+                      onClick={() => open(getApplications.data.demoUrl)}
+                    />
                   </ToolTip>
                 )}
               </div>
             </div>
-
-            <div className={styles.applicationImageContainer}>
-              <img src={tempApplications.detailPageImageUrl ?? grey} className={styles.applicationImage} />
-            </div>
           </div>
 
           <Divider />
+
+          <div className={styles.applicationImageWrapper}>
+            <Heading2>Screenshot</Heading2>
+            <img src={getApplications.data.detailPageImageUrl} className={styles.applicationImage} />
+          </div>
         </>
       )}
 
-      {!tempApplications && <Skeleton height="200px" />}
+      {getApplications.isLoading && <Skeleton height="200px" />}
     </Container>
   );
 };
