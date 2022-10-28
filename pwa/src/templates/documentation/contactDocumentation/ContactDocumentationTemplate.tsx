26cf0a3233c0f950432963583e7dd88a683793d0
Remko <remko@conduction.nl>
2022-10-28 15:37:53 +0200

    added text to contact page

--- pwa/src/templates/documentation/contactDocumentation/ContactDocumentationTemplate.tsx
+++ pwa/src/templates/documentation/contactDocumentation/ContactDocumentationTemplate.tsx
@@ -4,26 +4,54 @@ import { Container } from "@conduction/components";
 import { Heading1, LeadParagraph, Link } from "@gemeente-denhaag/components-react";
 import { navigate } from "gatsby";
 import { ExternalLinkIcon } from "@gemeente-denhaag/icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope, faPhone } from "@fortawesome/free-solid-svg-icons";
 
 export const ContactDocumentationTemplate: React.FC = () => {
   return (
     <Container layoutClassName={styles.container}>
       <section className={styles.section}>
-        <Heading1>Contact Documentatie</Heading1>
+        <Heading1 className={styles.title}>Contact</Heading1>
 
-        <LeadParagraph>
-          Quisque id mi. Vivamus in erat ut urna cursus vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus
-          et ultrices posuere cubilia Curae Sed aliquam.{" "}
+        <LeadParagraph className={styles.description}>
+          Wil je meer weten of heb je vragen dan kun je terecht bij de initiatiefnemers van dit project:{" "}
           <span
             onClick={() => {
-              navigate("#");
+              open("https://www.rotterdam.nl");
             }}
           >
             <Link icon={<ExternalLinkIcon />} iconAlign="start">
-              Lees meer.
+              De Gemeente Rotterdam
             </Link>
           </span>{" "}
-          Nisi quis porttitor congue, elit erat euismod orci, ac placerat dolor lectus quis orci.
+          en{" "}
+          <span
+            onClick={() => {
+              open("https://www.conduction.nl");
+            }}
+          >
+            <Link icon={<ExternalLinkIcon />} iconAlign="start">
+              Conduction
+            </Link>
+          </span>
+          . Dit kan je doen door te mailen naar{" "}
+          <span onClick={() => navigate(`mailto:info@conduction.nl`)}>
+            <Link icon={<FontAwesomeIcon icon={faEnvelope} />} iconAlign="start">
+              Conduction
+            </Link>
+          </span>{" "}
+          of te bellen naar{" "}
+          <span onClick={() => navigate(`tel:14010`)}>
+            <Link icon={<FontAwesomeIcon icon={faPhone} />} iconAlign="start">
+              De Gemeente Rotterdam
+            </Link>
+          </span>{" "}
+          of naar{" "}
+          <span onClick={() => navigate(`tel:+31853036840`)}>
+            <Link icon={<FontAwesomeIcon icon={faPhone} />} iconAlign="start">
+              Conduction
+            </Link>
+          </span>
         </LeadParagraph>
       </section>
     </Container>
