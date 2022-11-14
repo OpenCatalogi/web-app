d76ea10798828da0d169183cb46fe3829692bb21
Thijn <Thijn@conduction.nl>
2022-11-14 14:51:15 +0100

    Catagory cards have a "show more" button when in mobile mode

--- pwa/src/templates/templateParts/landingDisplayTemplates/categories/CategoriesLandingDisplayTemplate.tsx
+++ pwa/src/templates/templateParts/landingDisplayTemplates/categories/CategoriesLandingDisplayTemplate.tsx
@@ -8,17 +8,23 @@ import { navigate } from "gatsby";
 import { ArrowRightIcon } from "@gemeente-denhaag/icons";
 import { TEMPORARY_DOMAINS } from "../../../../data/domains";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTags } from "@fortawesome/free-solid-svg-icons";
+import { faChevronRight, faTags } from "@fortawesome/free-solid-svg-icons";
 import { TEMPORARY_PORTFOLIOS } from "../../../../data/portfolio";
+import Collapsible from "react-collapsible";
+import { GatsbyContext } from "../../../../context/gatsby";
+import clsx from "clsx";
 
 interface CategoriesLandingDisplayTemplateProps {}
 
 export const CategoriesLandingDisplayTemplate: React.FC<CategoriesLandingDisplayTemplateProps> = ({}) => {
   const { t } = useTranslation();
-  const [showMore, setShowMore] = React.useState(false);
 
   const domains = TEMPORARY_DOMAINS;
   const categories = TEMPORARY_PORTFOLIOS;
+
+  const [showMore, setShowMore] = React.useState(domains.map((item) => false));
+
+  const { screenSize } = React.useContext(GatsbyContext);
 
   return (
     <>
@@ -31,15 +37,30 @@ export const CategoriesLandingDisplayTemplate: React.FC<CategoriesLandingDisplay
       </div>
 
       <div className={styles.ComponentsGrid}>
-        {domains.map((domain) => (
+        {domains.map((domain, index) => (
           <>
-            {showMore && (
-              <CategoryCard
-                title={{ label: t(domain.title), href: `/categories#${domain.title}` }}
-                description={
-                  <div>
-                    {categories
+            <CategoryCard
+              title={{ label: t(domain.title), href: `/categories#${domain.title}` }}
+              description={
+                <div>
+                  {categories
+                    .filter((category) => category.domain === domain.title)
+                    .slice(0, 5)
+                    .map((_category) => (
+                      <div>
+                        <div onClick={() => navigate(`/categories/${_category.id}`)}>
+                          <Link icon={<ArrowRightIcon />} iconAlign="start">
+                            {_category.title}
+                          </Link>
+                        </div>
+                      </div>
+                    ))}
+
+                  {screenSize !== "mobile" &&
+                    categories.filter((category) => category.domain === domain.title).slice(5).length > 0 &&
+                    categories
                       .filter((category) => category.domain === domain.title)
+                      .slice(5)
                       .map((_category) => (
                         <div>
                           <div onClick={() => navigate(`/categories/${_category.id}`)}>
@@ -49,44 +70,45 @@ export const CategoriesLandingDisplayTemplate: React.FC<CategoriesLandingDisplay
                           </div>
                         </div>
                       ))}
+                  {screenSize === "mobile" &&
+                    categories.filter((category) => category.domain === domain.title).slice(5).length > 0 && (
+                      <>
+                        <Collapsible trigger={<></>} open={showMore[index]} transitionTime={200}>
+                          {categories
+                            .filter((category) => category.domain === domain.title)
+                            .slice(5)
+                            .map((_category) => (
+                              <div>
+                                <div onClick={() => navigate(`/categories/${_category.id}`)}>
+                                  <Link icon={<ArrowRightIcon />} iconAlign="start">
+                                    {_category.title}
+                                  </Link>
+                                </div>
+                              </div>
+                            ))}
+                        </Collapsible>
 
-                    <button className="btn" onClick={() => setShowMore(!showMore)}>
-                      {showMore ? "Show less" : "Show more"}
-                    </button>
-                  </div>
-                }
-                icon={<FontAwesomeIcon icon={faTags} />}
-                domain={true}
-              />
-            )}
-
-            {!showMore && (
-              <CategoryCard
-                title={{ label: t(domain.title), href: `/categories#${domain.title}` }}
-                description={
-                  <div>
-                    {categories
-                      .filter((category) => category.domain === domain.title)
-                      .slice(0, 5)
-                      .map((_category) => (
-                        <div>
-                          <div onClick={() => navigate(`/categories/${_category.id}`)}>
-                            <Link icon={<ArrowRightIcon />} iconAlign="start">
-                              {_category.title}
-                            </Link>
-                          </div>
-                        </div>
-                      ))}
-
-                    <button className="btn" onClick={() => setShowMore(!showMore)}>
-                      {showMore ? "Show less" : "Show more"}
-                    </button>
-                  </div>
-                }
-                icon={<FontAwesomeIcon icon={faTags} />}
-                domain={true}
-              />
-            )}
+                        <Button
+                          variant="secondary-action"
+                          onClick={() =>
+                            setShowMore(Object.values<boolean>({ ...showMore, [index]: !showMore[index] }))
+                          }
+                          icon={
+                            <FontAwesomeIcon
+                              className={clsx(styles.toggleIcon, showMore[index] && styles.isOpen)}
+                              icon={faChevronRight}
+                            />
+                          }
+                        >
+                          {showMore[index] ? "show less" : "show more"}
+                        </Button>
+                      </>
+                    )}
+                </div>
+              }
+              icon={<FontAwesomeIcon icon={faTags} />}
+              domain={true}
+            />
           </>
         ))}
       </div>
