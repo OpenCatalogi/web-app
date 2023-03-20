8f7c6fd663509a1313f10cdd2925631fd92f47b2
Rids <74712581+Ridsko@users.noreply.github.com>
2023-03-20 15:58:54 +0100

    ♻️Link component toegevoegd aan ApiDocumentation

--- pwa/src/templates/documentation/apiDocumentation/ApiDocumentationTemplate.tsx
+++ pwa/src/templates/documentation/apiDocumentation/ApiDocumentationTemplate.tsx
@@ -1,7 +1,8 @@
 import * as React from "react";
 import * as styles from "./ApiDocumentationTemplate.module.css";
 import { Container } from "@conduction/components";
-import { Heading1, LeadParagraph, Link } from "@gemeente-denhaag/components-react";
+import { Heading1, LeadParagraph } from "@gemeente-denhaag/components-react";
+import { Link, Icon } from "@utrecht/component-library-react/dist/css-module";
 import { navigate } from "gatsby";
 import { ExternalLinkIcon } from "@gemeente-denhaag/icons";
 
@@ -19,7 +20,10 @@ export const ApiDocumentationTemplate: React.FC = () => {
               navigate("#");
             }}
           >
-            <Link icon={<ExternalLinkIcon />} iconAlign="start">
+            <Link>
+              <Icon>
+                <ExternalLinkIcon />
+              </Icon>
               Lees meer.
             </Link>
           </span>{" "}
