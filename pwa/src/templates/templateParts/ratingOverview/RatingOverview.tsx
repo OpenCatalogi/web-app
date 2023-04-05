338fdbce06aa61d639ad23db7a04d9de17fa5e8d
Rids <74712581+Ridsko@users.noreply.github.com>
2023-04-05 15:40:53 +0200

    Table veranderd naar list

--- pwa/src/templates/templateParts/ratingOverview/RatingOverview.tsx
+++ pwa/src/templates/templateParts/ratingOverview/RatingOverview.tsx
@@ -2,13 +2,7 @@ import * as React from "react";
 import * as styles from "./RatingOverview.module.css";
 import { QueryObserverSuccessResult } from "react-query";
 import { t } from "i18next";
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHeaderCell,
-  TableRow,
-} from "@utrecht/component-library-react/dist/css-module";
+import { UnorderedList, UnorderedListItem } from "@utrecht/component-library-react/dist/css-module";
 
 interface RatingOverviewProps {
   getComponent: QueryObserverSuccessResult<any, Error>;
@@ -23,38 +17,35 @@ export const RatingOverview: React.FC<RatingOverviewProps> = ({ getComponent }) 
       {!getComponent.data.embedded?.rating?.rating && <span>{t("No rating available")}</span>}
 
       <div className={styles.popupDescription}>
-        <Table>
-          <TableBody>
-            {getComponent.data.embedded?.rating?.rating >= 1 && (
-              <>
-                <TableRow>
-                  <TableHeaderCell>Behaalde punten</TableHeaderCell>
-                </TableRow>
-                {getComponent.data.embedded?.rating?.results
-                  .filter((result: string) => !/^Cannot rate the/.test(result))
-                  .map((result: string) => (
-                    <TableRow>
-                      <TableCell>{result}</TableCell>
-                    </TableRow>
-                  ))}
-              </>
-            )}
-            {getComponent.data.embedded?.rating?.rating !== getComponent.data.embedded?.rating?.maxRating && (
-              <>
-                <TableRow>
-                  <TableHeaderCell>Onbehaalde punten</TableHeaderCell>
-                </TableRow>
-                {getComponent.data.embedded?.rating?.results
-                  .filter((result: string) => /^Cannot rate the/.test(result))
-                  .map((result: string) => (
-                    <TableRow>
-                      <TableCell>{result}</TableCell>
-                    </TableRow>
-                  ))}
-              </>
-            )}
-          </TableBody>
-        </Table>
+        <UnorderedList>
+          {getComponent.data.embedded?.rating?.rating >= 1 && (
+            <>
+              <li>Behaalde punten</li>
+
+              {getComponent.data.embedded?.rating?.results
+                .filter((result: string) => !/^Cannot rate the/.test(result))
+                .map((result: string) => (
+                  <ul>
+                    {" "}
+                    <li>{result}</li>
+                  </ul>
+                ))}
+            </>
+          )}
+          {getComponent.data.embedded?.rating?.rating !== getComponent.data.embedded?.rating?.maxRating && (
+            <>
+              <li>Onbehaalde punten</li>
+
+              {getComponent.data.embedded?.rating?.results
+                .filter((result: string) => /^Cannot rate the/.test(result))
+                .map((result: string) => (
+                  <ul>
+                    <li className={styles.customTable}>{result}</li>
+                  </ul>
+                ))}
+            </>
+          )}
+        </UnorderedList>
       </div>
     </>
   );
