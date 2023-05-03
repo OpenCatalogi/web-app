b2e8b472cb67f87d5c702d19947891de4046336e
Rids <74712581+Ridsko@users.noreply.github.com>
2023-05-03 15:34:17 +0200

    Work in progress

---
+++ pwa/src/components/icons/icons.tsx
@@ -0,0 +1,15 @@
+import { arrowright } from "../../assets/svgs/rotterdam/arrowright.svg";
+import { search } from "../../assets/svgs/rotterdam/search.svg";
+
+export default {
+  rotterdamIcons: [
+    {
+      icon: { arrowright },
+      desc: "Pijltje naar rechts icoontje",
+    },
+    {
+      icon: { search },
+      desc: "Vergrootglas icoontje",
+    },
+  ],
+};
