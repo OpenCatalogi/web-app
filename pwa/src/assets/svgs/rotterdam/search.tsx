fdf583f15a62de4cc5c79585034f39b336fd5045
Rida <targetagar@gmail.com>
2023-05-04 16:00:08 +0200

    SearchIcon rotterdam

---
+++ pwa/src/assets/svgs/rotterdam/search.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+
+export const SearchRotterdam = () => (
+  <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M26 13C26 9.1 22.9 6 19 6C15.1 6 12 9.1 12 13C12 16.9 15.1 20 19 20C22.9 20 26 16.9 26 13ZM9.99997 13C9.99997 8 14 4 19 4C24 4 28 8 28 13C28 18 24 22 19 22C16.8631 22 14.9089 21.2694 13.3715 20.0424L5.67641 27.7375L4.26221 26.3233L11.9573 18.6282C10.7305 17.0908 9.99997 15.1367 9.99997 13Z"
+      fill="#404B4F"
+    />
+  </svg>
+);
