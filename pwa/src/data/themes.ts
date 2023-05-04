9b61897d57d37ae9e70347862e3277c50b53cd26
scar055 <48519289+scar055@users.noreply.github.com>
2023-05-04 14:35:52 +0200

    🎨 rotterdam thema geïmporteert

--- pwa/src/data/themes.ts
+++ pwa/src/data/themes.ts
@@ -1,4 +1,5 @@
 export const themes = [
   { label: "Rotterdam", value: "rotterdam", className: "rotterdam-theme" },
   { label: "Utrecht", value: "utrecht", className: "utrecht-theme" },
+  { label: "Amsterdam", value: "amsterdam", className: "amsterdam-theme" },
 ];
