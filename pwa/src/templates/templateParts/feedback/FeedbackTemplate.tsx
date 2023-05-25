4c48aacdcb1c711e1b21406ca7fbd8247c70d9cb
Rids <74712581+Ridsko@users.noreply.github.com>
2023-05-25 14:51:28 +0200

    customcss en buttonlink appearance weggehaald

--- pwa/src/templates/templateParts/feedback/FeedbackTemplate.tsx
+++ pwa/src/templates/templateParts/feedback/FeedbackTemplate.tsx
@@ -17,14 +17,11 @@ export const FeedbackTemplate: React.FC<FeedbackTemplateProps> = ({ layoutClassN
         Iedereen die geïnteresseerd is in dit product kan de roadmap bekijken of zelf ideën aandragen.
       </span>
       <div className={styles.buttons}>
-        <ButtonLink
-          appearance="secondary-action-button"
-          href="https://github.com/OpenCatalogi/.github/issues/new?assignees=rubenvdlinde%2CRonaldvCortenberghe&labels=feature-request&template=feature.yml&title=%5Bfeature%5D%3A+Als+gebruiker+wil+ik+"
-        >
+        <ButtonLink href="https://github.com/OpenCatalogi/.github/issues/new?assignees=rubenvdlinde%2CRonaldvCortenberghe&labels=feature-request&template=feature.yml&title=%5Bfeature%5D%3A+Als+gebruiker+wil+ik+">
           <ExternalLinkIcon />
           Wens of opmerking indienen op GitHub
         </ButtonLink>
-        <ButtonLink appearance="secondary-action-button" href="https://github.com/orgs/OpenCatalogi/projects/1">
+        <ButtonLink href="https://github.com/orgs/OpenCatalogi/projects/1">
           <ExternalLinkIcon />
           Roadmap bekijken
         </ButtonLink>
