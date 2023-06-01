cd1e464f6ecf7d103ec56495097e4850768227b4
Rida <targetagar@gmail.com>
2023-06-01 07:38:09 +0200

    work in progress

--- pwa/src/data/themes.ts
+++ pwa/src/data/themes.ts
@@ -1,4 +1,5 @@
 export const themes = [
   { label: "Rotterdam", value: "rotterdam", className: "rotterdam-theme" },
   { label: "Utrecht", value: "utrecht", className: "utrecht-theme" },
+  { label: "Overheid.nl", value: "overheidnl", className: "overheidnl-theme" },
 ];
