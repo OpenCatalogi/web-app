8b99a739b5bb3baa1075506e8b277e9e696edf8d
Rida <targetagar@gmail.com>
2023-06-05 13:22:04 +0200

    Overheid theme added opencatalogi

--- pwa/src/data/themes.ts
+++ pwa/src/data/themes.ts
@@ -1,5 +1,5 @@
 export const themes = [
-  { label: "Overheid.nl", value: "overheidnl", className: "overheidnl-theme" },
   { label: "Rotterdam", value: "rotterdam", className: "rotterdam-theme" },
   { label: "Utrecht", value: "utrecht", className: "utrecht-theme" },
+  { label: "Overheid.nl", value: "overheidnl", className: "overheidnl-theme" },
 ];
