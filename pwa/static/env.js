8d25df3d1a20096ea89ccc35ddd894f9990f82e3
Remko <remko@conduction.nl>
2023-06-19 11:25:08 +0200

    prettier local fix

--- pwa/static/env.js
+++ pwa/static/env.js
@@ -2,13 +2,19 @@ window.sessionStorage.setItem(
   "GATSBY_ME_URL",
   "https://api.opencatalogi.nl/api/users/me"
 );
-window.sessionStorage.setItem("GATSBY_API_URL", "https://api.opencatalogi.nl/api");
+window.sessionStorage.setItem(
+  "GATSBY_API_URL",
+  "https://api.opencatalogi.nl/api"
+);
 window.sessionStorage.setItem(
   "GATSBY_ADMIN_URL",
   "https://api.opencatalogi.nl/admin"
 );
 window.sessionStorage.setItem("GATSBY_BASE_URL", "https://api.opencatalogi.nl");
-window.sessionStorage.setItem("GATSBY_FRONTEND_URL", "https://api.opencatalogi.nl");
+window.sessionStorage.setItem(
+  "GATSBY_FRONTEND_URL",
+  "https://api.opencatalogi.nl"
+);
 window.sessionStorage.setItem("GATSBY_ORGANIZATION", "");
 window.sessionStorage.setItem("GATSBY_LOGIN_REDIRECT", "vault");
 window.sessionStorage.setItem(
