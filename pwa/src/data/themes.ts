46edb5722910b0356395a7344b02e907f4b20e72
Remko <remko@conduction.nl>
2023-06-21 14:06:29 +0200

    hoorn theme

--- pwa/src/data/themes.ts
+++ pwa/src/data/themes.ts
@@ -2,4 +2,5 @@ export const themes = [
   { label: "Rotterdam", value: "rotterdam", className: "rotterdam-theme" },
   { label: "Utrecht", value: "utrecht", className: "utrecht-theme" },
   { label: "Zwolle", value: "zwolle", className: "zwolle-theme" },
+  { label: "Hoorn", value: "hoorn", className: "hoorn-theme" },
 ];
