d9f57341449359276a08bc6d9333575e84a05cc7
Lennart Bank <contact@lencodes.com>
2023-06-26 13:34:53 +0200

    ADD ExpandableDescription component

---
+++ pwa/src/components/expandableDescription/ExpandableDescription.tsx
@@ -0,0 +1,46 @@
+import * as React from "react";
+import * as styles from "./ExpandableDescription.module.css";
+import { Button, Paragraph } from "@utrecht/component-library-react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
+import clsx from "clsx";
+
+const SHORT_DESCRIPTION_TRESHOLD = 300;
+
+interface ExpandableDescriptionProps {
+  description: string;
+}
+
+export const ExpandableDescription: React.FC<ExpandableDescriptionProps> = ({ description }) => {
+  const [isExpanded, setIsExpanded] = React.useState<boolean>(false);
+  const [shortDescription, setShortDescription] = React.useState<string>("");
+  const [descriptionIsExpandable, setDescriptionIsExpandable] = React.useState<boolean>(false);
+
+  React.useEffect(() => {
+    setDescriptionIsExpandable(description.length > SHORT_DESCRIPTION_TRESHOLD);
+  }, [description]);
+
+  React.useEffect(() => {
+    if (!descriptionIsExpandable) return;
+
+    setShortDescription(`${description.substring(0, SHORT_DESCRIPTION_TRESHOLD)}...`);
+  }, [descriptionIsExpandable]);
+
+  if (!descriptionIsExpandable) return <Paragraph lead>{description}</Paragraph>; // Show desciption as-is without any changes
+
+  return (
+    <div className={styles.container}>
+      <Paragraph lead>{isExpanded ? description : shortDescription}</Paragraph>
+
+      <Button
+        appearance="secondary-action-button"
+        className={clsx(styles.toggleButton, isExpanded && styles.isExpanded)}
+        onClick={() => setIsExpanded((isExpanded) => !isExpanded)}
+      >
+        {isExpanded ? "Omschrijving inklappen" : "Volledige omschrijving lezen"}
+
+        <FontAwesomeIcon icon={faChevronDown} />
+      </Button>
+    </div>
+  );
+};
