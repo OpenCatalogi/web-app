2bb1c6fd820fb8449654385fa72818ccf41487e9
Lennart Bank <contact@lencodes.com>
2023-09-12 15:15:39 +0200

    INIT buttons scroll

--- pwa/src/components/tableWrapper/TableWrapper.tsx
+++ pwa/src/components/tableWrapper/TableWrapper.tsx
@@ -1,6 +1,52 @@
-import * as React from "react";
+import React, { useState, useEffect } from "react";
 import * as styles from "./TableWrapper.module.css";
+import { Button } from "@utrecht/component-library-react";
 
 export const TableWrapper: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-  return <div className={styles.wrapper}>{children}</div>;
+  const [canScrollRight, setCanScrollRight] = useState(false);
+  const [canScrollLeft, setCanScrollLeft] = useState(false);
+
+  const wrapperRef = React.useRef<HTMLDivElement | null>(null);
+
+  const handleScroll = () => {
+    if (wrapperRef.current) {
+      setCanScrollLeft(wrapperRef.current.scrollLeft > 0);
+      setCanScrollRight(
+        wrapperRef.current.scrollWidth - wrapperRef.current.scrollLeft > wrapperRef.current.clientWidth,
+      );
+    }
+  };
+
+  const handleScrollRight = () => {
+    if (wrapperRef.current) wrapperRef.current.scrollTo({ left: wrapperRef.current.scrollWidth, behavior: "smooth" });
+  };
+
+  const handleScrollLeft = () => {
+    if (wrapperRef.current) wrapperRef.current.scrollTo({ left: 0, behavior: "smooth" });
+  };
+
+  useEffect(() => {
+    if (wrapperRef.current) {
+      setCanScrollRight(wrapperRef.current.scrollWidth > wrapperRef.current.clientWidth); // initiate scroll
+    }
+  }, []);
+
+  return (
+    <div className={styles.container}>
+      <div onScroll={handleScroll} ref={wrapperRef} className={styles.wrapper}>
+        {children}
+      </div>
+
+      {canScrollLeft && (
+        <div onClick={handleScrollLeft} className={styles.scrollLeftButton}>
+          <Button>Left</Button>
+        </div>
+      )}
+      {canScrollRight && (
+        <div onClick={handleScrollRight} className={styles.scrollRightButton}>
+          <Button>Right</Button>
+        </div>
+      )}
+    </div>
+  );
 };
