abe292758ae00668563c0f83ff05aabf9e803998
Lennart Bank <contact@lencodes.com>
2023-09-27 09:48:16 +0200

    INIT deployment workflow

--- pwa/gatsby-config.js
+++ pwa/gatsby-config.js
@@ -5,6 +5,10 @@ require("dotenv").config({
 });
 
 module.exports = {
+  pathPrefix:
+    process.env.USE_GITHUB_REPOSITORY_NAME_AS_PATH_PREFIX === "true"
+      ? `/${process.env.GITHUB_REPOSITORY_NAME}`
+      : "", // we do NOT want to set the prefix if we're using an DNS
   plugins: [
     {
       resolve: `gatsby-plugin-layout`,
