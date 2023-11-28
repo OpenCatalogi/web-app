44a6a19cd3ec65bbb46dca780d2fb5d8cb5ac968
Lencodes <contact@lencodes.com>
2023-11-28 15:04:40 +0100

    REFACTOR Head to work with sessionStorage

--- pwa/src/layout/Head.tsx
+++ pwa/src/layout/Head.tsx
@@ -4,20 +4,22 @@ import { Helmet } from "react-helmet";
 import { useInitCssVariables } from "../hooks/initCssVariables";
 
 export const Head: React.FC = () => {
-  const theme = process.env.GATSBY_NL_DESIGN_THEME_CLASSNAME ?? "conduction-theme";
+  const [theme, setTheme] = React.useState<string>("conduction-theme");
+  const [title, setTitle] = React.useState<string>("OpenCatalogi");
+
+  React.useEffect(() => {
+    setTheme(window.sessionStorage.getItem("NL_DESIGN_THEME_CLASSNAME") ?? "conduction-theme");
+    setTitle(window.sessionStorage.getItem("PAGE_TITLE") ?? "OpenCatalogi");
+  }, []);
+
   useInitCssVariables(theme);
 
   return (
     <Helmet
+      {...{ title }}
       bodyAttributes={{
         class: theme,
       }}
-    >
-      <title>
-        {process.env.GATSBY_PAGE_TITLE && process.env.GATSBY_PAGE_TITLE !== ""
-          ? process.env.GATSBY_PAGE_TITLE
-          : "OpenCatalogi"}
-      </title>
-    </Helmet>
+    />
   );
 };
