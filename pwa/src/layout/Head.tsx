3b870b39181d702ee176c11c075bef9709e9a216
Lencodes <contact@lencodes.com>
2023-11-28 17:52:41 +0100

    BUGFIX ensure className is updated via sessionStorage

--- pwa/src/layout/Head.tsx
+++ pwa/src/layout/Head.tsx
@@ -4,21 +4,17 @@ import { Helmet } from "react-helmet";
 import { useInitCssVariables } from "../hooks/initCssVariables";
 
 export const Head: React.FC = () => {
-  const [theme, setTheme] = React.useState<string>("conduction-theme");
-  const [title, setTitle] = React.useState<string>("OpenCatalogi");
-
-  React.useEffect(() => {
-    setTheme(window.sessionStorage.getItem("NL_DESIGN_THEME_CLASSNAME") ?? "conduction-theme");
-    setTitle(window.sessionStorage.getItem("PAGE_TITLE") ?? "OpenCatalogi");
-  }, []);
+  const [theme] = React.useState<string>(
+    window.sessionStorage.getItem("NL_DESIGN_THEME_CLASSNAME") ?? "conduction-theme",
+  );
 
   useInitCssVariables(theme);
 
   return (
     <Helmet
-      {...{ title }}
+      title={window.sessionStorage.getItem("PAGE_TITLE") ?? "OpenCatalogi"}
       bodyAttributes={{
-        class: theme,
+        class: window.sessionStorage.getItem("NL_DESIGN_THEME_CLASSNAME") ?? "conduction-theme",
       }}
     />
   );
