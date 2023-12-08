e803f58838ea801550b7bcabcb65d7d98e2d761a
Remko <remko@conduction.nl>
2023-12-08 16:05:02 +0100

    added more config files

--- pwa/src/services/getConfig.ts
+++ pwa/src/services/getConfig.ts
@@ -3,6 +3,11 @@ import { TGroupedSelectOption } from "@conduction/components/lib/components/form
 import Conduction from "./../../static/configFiles/conduction.json";
 import CommonGround from "./../../static/configFiles/common-ground.json";
 import Rotterdam from "./../../static/configFiles/rotterdam.json";
+import Epe from "./../../static/configFiles/epe.json";
+import Noordwijk from "./../../static/configFiles/noordwijk.json";
+import Leiden from "./../../static/configFiles/leiden.json";
+import Dimpact from "./../../static/configFiles/dimpact.json";
+import OpenWebconcept from "./../../static/configFiles/open-webconcept.json";
 
 export const getConfig = (themeOrDomainName: string): Record<string, any> | undefined => {
   switch (themeOrDomainName) {
@@ -13,6 +18,16 @@ export const getConfig = (themeOrDomainName: string): Record<string, any> | unde
       return Rotterdam;
     case "conduction-theme":
       return Conduction;
+    case "epe-theme":
+      return Epe;
+    case "noordwijk-theme":
+      return Noordwijk;
+    case "leiden-theme":
+      return Leiden;
+    case "dimpact-theme":
+      return Dimpact;
+    case "open-webconcept-theme":
+      return OpenWebconcept;
 
     default:
       return CommonGround;
@@ -26,6 +41,11 @@ export const availableThemes: TGroupedSelectOption[] = [
       { label: "Common Ground", value: "commonground-theme" },
       { label: "Conduction", value: "conduction-theme" },
       { label: "Rotterdam", value: "rotterdam-theme" },
+      { label: "Epe", value: "epe-theme" },
+      { label: "Noordwijk", value: "noordwijk-theme" },
+      { label: "Leiden", value: "leiden-theme" },
+      { label: "Dimpact", value: "dimpact-theme" },
+      { label: "OpenWebconcept", value: "open-webconcept-theme" },
     ],
   },
 ];
